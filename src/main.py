47eaebff863917c11ae63ef69ef536ae64676e18
RietzF <fredbanksdurietz@gmail.com>
2025-04-23 18:51:49 -0700

    D: Completed TextNode

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+from textnode import *
 def main():
-    print("hello world")
+    print(TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev'))
 
 if __name__ == '__main__':
     main()
