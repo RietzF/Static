a65241f1a4bd1d4d065bbec26acd4ae66ff1e5a4
RietzF <fredbanksdurietz@gmail.com>
2025-04-25 22:35:45 -0700

    G: new stuff added

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import *
+from textnode import *
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_with_single_prop(self):
@@ -41,3 +42,9 @@ class TestParentnode(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
         )
+
+def test_text(self):
+    node = TextNode("This is a text node", TextType.TEXT)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, None)
+    self.assertEqual(html_node.value, "This is a text node")
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 class TextType(Enum):
     NORMAL = "normal"
     BOLD = 'bold'
@@ -23,8 +24,16 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
 def text_node_to_html_node(text_node):
-    match text_node:
-        case text_node.TextType.NORMAL:
-            pass
-
-
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
